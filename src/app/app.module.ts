d0765de171cfee2dd36cfbf06144b45801968511
Nakito37 <mazin37.fa@gmail.com>
2024-09-11 23:43:26 -0600

    components, pages & services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PrincipalComponent } from './pages/principal/principal.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { PublicationComponent } from './pages/publication/publication.component';
+import { VerificationComponent } from './components/verification/verification.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    FooterComponent,
+    PrincipalComponent,
+    ProfileComponent,
+    PublicationComponent,
+    VerificationComponent,
+    ResetPasswordComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
